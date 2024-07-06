f415a119ffa0ce4110766198032ac138d8f286ca
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-07-06 14:01:36 +0000

    added jest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function Home() {
+function Home() {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 text-center">
-      <h1 className="text-4xl font-bold">BMI Calculator App</h1>
+    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 py-10 text-center">
+      <h1 className="text-3xl font-bold text-gray-800">BMI Calculator App</h1>
     </div>
   );
 }
+
+export default Home;
